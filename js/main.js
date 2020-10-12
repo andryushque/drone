6ddb937a377509b3d488379f6b9559df4268a8ab
andryushque <andrew.ustinov23@gmail.com>
2020-10-12 12:33:05 +0300

    Create: media for header section + mobile menu

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
 $(document).ready(function () {
-  // js code
+  /*=== 01. Slick Nav ===*/
+  // mobile_menu
+    const menu = $('ul#header-navigation');
+    if(menu.length){
+      menu.slicknav({
+        prependTo: ".mmenu",
+        closedSymbol: '+',
+        openedSymbol: '-'
+      });
+    };
 });
