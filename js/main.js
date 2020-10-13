3174997f26e58dac2c5f67cce8f71804eb50a4a0
andryushque <andrew.ustinov23@gmail.com>
2020-10-13 11:30:24 +0300

    Create: services section

--- js/main.js
+++ js/main.js
@@ -9,4 +9,28 @@ $(document).ready(function () {
         openedSymbol: '-'
       });
     };
+
+  /*=== 02. Swiper Slider ===*/
+  // Slider in services section
+  const servicesSlider = new Swiper(".services-slider", {
+    speed: 400,
+    loop: true,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+      },
+      767: {
+        slidesPerView: 2,
+        spaceBetween: 30,
+      },
+      991: {
+        slidesPerView: 3,
+        spaceBetween: 45,
+      },
+    },
+  });
 });
