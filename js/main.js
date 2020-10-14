c6237be7d539eb912c9669d5e93519e664199963
andryushque <andrew.ustinov23@gmail.com>
2020-10-14 11:35:14 +0300

    Create: testimonials section

--- js/main.js
+++ js/main.js
@@ -16,8 +16,35 @@ $(document).ready(function () {
     speed: 400,
     loop: true,
     navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
+      nextEl: '.services-slide__btn-next',
+      prevEl: '.services-slide__btn-prev',
+    },
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+      },
+      767: {
+        slidesPerView: 2,
+        spaceBetween: 30,
+      },
+      991: {
+        slidesPerView: 3,
+        spaceBetween: 30,
+      },
+      1200: {
+        slidesPerView: 3,
+        spaceBetween: 45,
+      },
+    },
+  });
+
+  // Slider in testimonials section
+  const testimonialsSlider = new Swiper(".testimonials-slider", {
+    speed: 400,
+    loop: true,
+    navigation: {
+      nextEl: '.testimonials-slide__btn-next',
+      prevEl: '.testimonials-slide__btn-prev',
     },
     breakpoints: {
       320: {
